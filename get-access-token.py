597b7336778c23cb29109ddf96b3d19cefd3a890
Harrison Dews <harrisondews@gmail.com>
2017-02-21 14:43:19 -0500

    cleaned up

--- get-access-token.py
+++ get-access-token.py
@@ -8,40 +8,29 @@ class MyHTMLParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
     
-    def read(self, data):
-        self.script_data = []
-        self.reset()
-        self.feed(data)
-        return self.script_data
-    
     def handle_starttag(self, tag, attrs):
-        return tag
+        print "Start tag:", tag
+        for attr in attrs:
+            print "     attr:", attr
 
     def handle_endtag(self, tag):
         return tag
 
     def handle_data(self, data):
-        if 'accessToken' in data:
-            print "found it!"
-            self.script_data.append(data)
+        print data
         return data
-
+    
     def handle_comment(self, comment):
-        if 'accessToken' in comment:
-            print "found it!"
-            self.script_data.append(data)
         return comment
 
 #get response from server
-response = urlopen('https://developers.facebook.com/tools/explorer')
+response = urlopen('http://localhost/login.html')
 
 #read the data 
 html = response.read()
 
 #instantiate the parser and feed it html
 parser = MyHTMLParser()
+parser.feed(html)
 
-#read returns all data within a <script> tag
-data = parser.read(html)
-#print data
-#{"accessToken":null,
+#using selenium might be a better idea?
