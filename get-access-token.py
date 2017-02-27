123778caf32889faa18e252d188de5d127d0f13f
Harrison Dews <harrisondews@gmail.com>
2017-02-26 19:48:23 -0500

    gets token!

--- get-access-token.py
+++ get-access-token.py
@@ -1,36 +1,45 @@
-from urllib2 import urlopen #opens webpage
-from HTMLParser import HTMLParser
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+#the login page location (lives in an apache server on the local network)
+LOGIN_URL = "http://192.168.1.124/login.html"
+
+#username and password
+usr = ""
+pw = ""
+
+#init webdriver
+driver = webdriver.Firefox()
+
+#login
+driver.get(LOGIN_URL)
+main_handle = driver.current_window_handle
+for handle in driver.window_handles:       #gets a handle to the popup
+    if handle != main_handle:
+        signin_handle = handle
+        break
+sleep(1)                                   #sleep so popup can load
+driver.switch_to.window(signin_handle)     #switch to popup
+
+#do the login
+elem = driver.find_element_by_id('email')
+elem.send_keys(usr)
+elem = driver.find_element_by_id('pass')
+elem.send_keys(pw)
+elem = driver.find_element_by_id('loginbutton')
+elem.click()
+sleep(3)
+
+#switch back to main window
+driver.switch_to.window(main_handle)
+sleep(2)
+elem = driver.find_element_by_id('token')
+print elem.get_attribute('innerHTML')
 
 
+#elem = driver.find_element_by_id('token')
+#print elem
 
-#define a parser for the html we'll read later
-class MyHTMLParser(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-    
-    def handle_starttag(self, tag, attrs):
-        print "Start tag:", tag
-        for attr in attrs:
-            print "     attr:", attr
-
-    def handle_endtag(self, tag):
-        return tag
-
-    def handle_data(self, data):
-        print data
-        return data
-    
-    def handle_comment(self, comment):
-        return comment
-
-#get response from server
-response = urlopen('http://localhost/login.html')
-
-#read the data 
-html = response.read()
-
-#instantiate the parser and feed it html
-parser = MyHTMLParser()
-parser.feed(html)
 
 #using selenium might be a better idea?
